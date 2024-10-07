8c422a13f59da6c7dfc130d49ea1bd2ba0902505
fahd <faguenouz@gmail.com>
2024-10-07 12:38:43 +0100

    adjusting the result display

--- main.go
+++ main.go
@@ -17,59 +17,61 @@ type Data struct {
 	A	template.HTML
 }
 
+
 func processHandler(w http.ResponseWriter, r *http.Request) {
 	temp, err := template.ParseFiles("home.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	var  data Data
+
+	var data Data
 	data.Str = r.FormValue("data")
 	if len(data.Str) > 200 {
 		http.Error(w, "Input data exceeds 200 characters limit.", http.StatusBadRequest)
 		return
 	}
+
 	data.Banner = r.FormValue("banner")
-	if !function.BannerExists(data.Banner) {  // Assuming BannerExists checks for the validity of banners
-		http.Error(w, "Banner not found", http.StatusNotFound) // Return 404 if banner is not found
+	if !function.BannerExists(data.Banner) {
+		http.Error(w, "Banner not found", http.StatusNotFound)
 		return
 	}
+
 	data.Str = strings.ReplaceAll(data.Str, "\r\n", "\n")
-	fmt.Println(data.Str)
+	
 	data.Res = function.TraitmentData(data.Banner, data.Str)
 	if data.Res == "" { // If TraitmentData failed to generate the result
 		http.Error(w, "Internal Server Error: Failed to process data.", http.StatusInternalServerError)
 		return
 	}
 	data.A = template.HTML(strings.ReplaceAll(data.Res, "\n", "<br>"))
-	// temp.Execute(w, data)
+
 	if err := temp.Execute(w, data); err != nil {
-		http.Error(w,"Internal Server Error", http.StatusInternalServerError)
-		// Return 500 if template execution fails
-		return 
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
 
-func pageHandeler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/"{
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
 		http.Error(w, "Error 404 : Not Found", http.StatusNotFound)
 		return
 	}
 	t, err := template.ParseFiles("home.html")
 	if err != nil {
-		http.Error(w, "Template not found", http.StatusNotFound) // Return 404 Not Found
+		http.Error(w, "Template not found", http.StatusNotFound)
 		return
 	}
-	
+
 	if err := t.Execute(w, nil); err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError) // Return 500 if template execution fails
-		return 
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 }
-
-
 func main() {
 
-	http.HandleFunc("/", pageHandeler)
+	http.HandleFunc("/", pageHandler)
 	http.HandleFunc("/ascii-art", processHandler)
 	fmt.Println("Server is running at http://localhost:8088")
 	err := http.ListenAndServe(":8088", nil)
